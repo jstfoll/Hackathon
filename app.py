48e12759336cadf76046a8b6f143237c4b26f812
Sai Phani Sharath Chandra Danthalapelli <sai.phani.sharath.chandra.danthalapelli@sap.com>
2019-11-04 12:53:43 +0530

    server

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,request,url_for,redirect,render_template,flash, send_from_directory
+import os
+import jinja2
+import json
+import datetime
+from werkzeug.utils import secure_filename
+from pathlib import Path
+import platform
+import requests
+
+app = Flask(__name__, static_url_path='')
+if platform.system() == 'Windows':
+    UPLOAD_FOLDER = Path(__file__).parent.joinpath('uploads')
+else:
+    UPLOAD_FOLDER = r'uploads'
+
+
+
+
+@app.route('/')
+def homepage():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    pass
+
+
+
+
+	
+if __name__=='__main__':
+	app.run(host='0.0.0.0', port=8080, debug=True)
